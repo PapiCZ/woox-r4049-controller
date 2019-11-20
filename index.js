2e15d0f7d010c1d5d77ee7566d272d8de57984ff
Patrik Janoušek <patrikjanousek97@gmail.com>
2019-11-21 00:44:08 +0100

    Implemented on/off

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ device.find().then(() => {
 });
 
 device.on('connected', () => {
-    device.set({
-        multiple: true, 
-        data: {
-            '1': true,
-            '2': 'colour',
-            '5': process.argv[2].slice(0, 6) + '00000000'
-        }
-    });
+    if(process.argv[2] == 'off') {
+        device.set({set: false})
+    } else if(process.argv[2] == 'on') {
+        device.set({set: true})
+    } else {
+        device.set({
+            multiple: true, 
+            data: {
+                '1': true,
+                '2': 'colour',
+                '5': process.argv[2].slice(0, 6) + '00000000'
+            }
+        });
+    }
 });
 
 device.on('data', data => {
